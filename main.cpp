ab5287b90bd33ae293ff249efe26e61ebb58224b
cha <eve0110010010@gmail.com>
2024-05-10 14:40:34 +0800

    Print basic prompt and take command
    Co-authored-by: luvinne <luvinne4291@gmail.com>

--- main.cpp
+++ main.cpp
@@ -16,6 +16,25 @@ struct TodoItem {
 
 int main() 
 {
+    int command;
+
+    cout << "1. Add Task" << endl;
+    cout << "2. View Tasks" << endl;
+    cout << "3. Mark Task as Completed" << endl;
+    cout << "4. Edit Task" << endl;
+    cout << "5. Delete Task" << endl;
+    cout << "6. Exit" << endl << endl;
+
+    cout << "Enter a number 1-6: ";
+
+    while (true)
+    {
+        cin >> command;
+        if (command >= 1 && command <= 6)
+            break;
+        cout << "Please enter a number within range 1-6: ";
+    }
+
     return 0;
 }
 
